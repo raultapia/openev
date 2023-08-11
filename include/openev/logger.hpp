0fa949b1db8cd6757690995248f6c352df2da2f9
Raul Tapia <raultapia@us.es>
2023-08-11 22:14:12 +0200

    Add logger

---
+++ include/openev/logger.hpp
@@ -0,0 +1,40 @@
+/*!
+\file logger.hpp
+\brief Logger utility.
+\author Raul Tapia
+*/
+#ifndef OPENEV_LOGGER_HPP
+#define OPENEV_LOGGER_HPP
+
+#include <iostream>
+#include <memory>
+
+namespace ev::logger {
+
+/*!
+\brief Log message at info level.
+\param message Message
+*/
+inline void info(const char *message) {
+  std::cout << "INFO. openev: " << message << std::endl;
+}
+
+/*!
+\brief Log message at warning level.
+\param message Message
+*/
+inline void warning(const char *message) {
+  std::cout << "WARNING. openev: " << message << std::endl;
+}
+
+/*!
+\brief Log message at error level.
+\param message Message
+*/
+inline void error(const char *message) {
+  throw std::runtime_error("ERROR. openev: " + std::string(message, std::allocator<char>()));
+}
+
+} // namespace ev::logger
+
+#endif // OPENEV_LOGGER_HPP
