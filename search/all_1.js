c75598ebaa69933d19107b9c8247e1f282071739
raultapia <raultapia@users.noreply.github.com>
2024-05-06 18:20:08 +0000

    deploy: 7316b0dfea49ba782648817550bccb800a426ac2

--- search/all_1.js
+++ search/all_1.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['bias_0',['Bias',['../structBias.html',1,'']]],
   ['biasvalue_1',['BiasValue',['../structev_1_1BiasValue.html',1,'ev']]],
-  ['bilinearvoting_2',['bilinearVoting',['../classev_1_1AugmentedEvent__.html#a24f90d7fd6b6a09ae9fd9cd4554dbbd4',1,'ev::AugmentedEvent_']]]
+  ['bilinearvoting_2',['bilinearVoting',['../classev_1_1Event__.html#a9469861fe0af209b0f981cb81b40481d',1,'ev::Event_::bilinearVoting()'],['../classev_1_1AugmentedEvent__.html#a24f90d7fd6b6a09ae9fd9cd4554dbbd4',1,'ev::AugmentedEvent_::bilinearVoting()']]]
 ];
