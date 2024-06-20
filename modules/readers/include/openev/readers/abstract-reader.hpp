23cdb4a02c9bc3a84b8180c07686f7dcdad402de
Raul Tapia <raultapia@us.es>
2024-06-20 13:15:44 +0200

    Improve abstract reader

--- modules/readers/include/openev/readers/abstract-reader.hpp
+++ modules/readers/include/openev/readers/abstract-reader.hpp
@@ -6,10 +6,11 @@
 #ifndef OPENEV_READERS_ABSTRACT_READER_HPP
 #define OPENEV_READERS_ABSTRACT_READER_HPP
 
-#include "openev/containers.hpp"
+#include "openev/containers/array.hpp"
+#include "openev/containers/queue.hpp"
+#include "openev/containers/vector.hpp"
 #include "openev/core/types.hpp"
-#include <fstream>
-#include <sstream>
+#include <cstddef>
 
 namespace ev {
 
@@ -28,21 +29,13 @@ public:
   /*! \endcond */
 
   /*!
-  \brief Start reading from the first event in the dataset.
-  \note The behaviour of reset should be implemented in the derived classes.
-  */
-  inline void reset() {
-    return reset_();
-  }
-
-  /*!
   \brief Get next event from the dataset.
   \param e Event
   \return True if new event available
   \note The behaviour of next should be implemented in the derived classes.
   */
   inline bool next(Event &e) {
-    return next_(e);
+    return read_(e);
   }
 
   /*!
@@ -52,40 +45,86 @@ public:
   */
   template <std::size_t N>
   bool next(Array<N> &array) {
-    if constexpr(N < 1) {
-      return true;
-    }
-
-    ev::Event e;
-    for(std::size_t i = 0; i < N; i++) {
-      if(!next_(e)) {
+    for(Event &a : array) {
+      if(!read_(a)) {
         return false;
       }
-      array[i] = e;
     }
     return true;
   }
 
   /*!
   \brief Get next n events from the dataset.
-  \param n Number of events to get
   \param vector Event vector
+  \param n Number of events to get
   \return True if vector populated with n new events
   */
-  bool next(int n, Vector &vector);
+  bool next_n(Vector &vector, const int n);
 
   /*!
   \brief Get next n events from the dataset.
   \param n Number of events to get
   \param queue Event queue
-  \param keep_same_size Pop one event for each insertion
+  \param keep_size If true, pop one event for each insertion to maintain queue size.
   \return True if queue populated with n new events
   */
-  bool next(int n, Queue &queue, const bool keep_same_size = false);
+  bool next_n(Queue &queue, const int n, const bool keep_size = false);
+
+  /*!
+  \brief Get the next events until the specified duration is reached.
+  \param vector Event vector to store the events.
+  \param t Duration to get events for.
+  \return True if the vector is populated with events for the specified duration.
+  */
+  bool next_t(Vector &vector, const double t);
+
+  /*!
+  \brief Get the next events until the specified duration is reached and store them in a queue.
+  \param t Duration to get events for.
+  \param queue Event queue to store the events.
+  \param keep_size If true, pop one event for each insertion to maintain queue size.
+  \return True if the queue is populated with events for the specified duration.
+  */
+  bool next_t(Queue &queue, const double t, const bool keep_size = false);
+
+  /*!
+  \brief Skip the next n events in the dataset.
+  \param n Number of events to skip.
+  \return True if the skip was successful.
+  */
+  bool skip_n(int n);
+
+  /*!
+  \brief Skip events for the specified duration.
+  \param t Duration to skip events for.
+  \return True if the skip was successful.
+  */
+  bool skip_t(const double t);
+
+  /*!
+  \brief Start reading from the first event in the dataset.
+  \note The behaviour of reset should be implemented in the derived classes.
+  */
+  inline void reset() {
+    return reset_();
+  }
+
+  /*!
+  TODO
+  */
+  std::size_t count() {
+    std::size_t cnt = 0;
+    reset_();
+    while(skip_n(1)) {
+      cnt++;
+    }
+    reset_();
+    return cnt;
+  }
 
 protected:
+  virtual bool read_(Event &e) = 0;
   virtual void reset_() = 0;
-  virtual bool next_(Event &e) = 0;
 };
 
 } // namespace ev
