2b0e4e98d5885f8a98179ad7a0ad24ac1b097380
raultapia <raultapia@users.noreply.github.com>
2024-08-30 11:44:17 +0000

    deploy: bb273b8d03ffaf1f1149a35f15807eb8fbb5ab7f

--- search/all_2.js
+++ search/all_2.js
@@ -7,7 +7,7 @@ var searchData=
   ['circf_4',['Circf',['../types_8hpp.html#a7cbc0afa41025d180501299639207516',1,'ev']]],
   ['circi_5',['Circi',['../types_8hpp.html#a5128a9f25d86efd651c7411623176a7c',1,'ev']]],
   ['circl_6',['Circl',['../types_8hpp.html#a3e37ec53b152b464865ffebb723aeb08',1,'ev']]],
-  ['clear_7',['clear',['../classev_1_1AbstractRepresentation__.html#a1529e7c4143e6ebfe7ecde46c65ba9ea',1,'ev::AbstractRepresentation_']]],
+  ['clear_7',['clear',['../classev_1_1AbstractRepresentation__.html#a1529e7c4143e6ebfe7ecde46c65ba9ea',1,'ev::AbstractRepresentation_::clear()'],['../classev_1_1AbstractRepresentation__.html#a5b915d253af03d50af0f91d1007a4f95',1,'ev::AbstractRepresentation_::clear(const cv::Mat &amp;background)']]],
   ['contact_8',['📬 Contact',['../index.html#autotoc_md4',1,'']]],
   ['containers_2ehpp_9',['containers.hpp',['../containers_8hpp.html',1,'']]],
   ['contains_10',['contains',['../classev_1_1Rect3__.html#a9ffa96877ee35795b9159f6982b27ff7',1,'ev::Rect3_::contains()'],['../structev_1_1Circ__.html#a6287aaef59e3f5f63c32c8ec920a65a1',1,'ev::Circ_::contains()'],['../classev_1_1PointCloud__.html#a025d618422c0e4ff20f4cd86268e482c',1,'ev::PointCloud_::contains()']]],
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['circ_5f_0',['Circ_',['../structev_1_1Circ__.html#a07b7b8eec44d781ea87ae1ec9fd83b1d',1,'ev::Circ_::Circ_()'],['../structev_1_1Circ__.html#a19186a425d8487f0e147b34ed00eb831',1,'ev::Circ_::Circ_(const cv::Point_&lt; T &gt; center, const T radius)']]],
-  ['clear_1',['clear',['../classev_1_1AbstractRepresentation__.html#a1529e7c4143e6ebfe7ecde46c65ba9ea',1,'ev::AbstractRepresentation_']]],
+  ['clear_1',['clear',['../classev_1_1AbstractRepresentation__.html#a1529e7c4143e6ebfe7ecde46c65ba9ea',1,'ev::AbstractRepresentation_::clear()'],['../classev_1_1AbstractRepresentation__.html#a5b915d253af03d50af0f91d1007a4f95',1,'ev::AbstractRepresentation_::clear(const cv::Mat &amp;background)']]],
   ['contains_2',['contains',['../classev_1_1Rect3__.html#a9ffa96877ee35795b9159f6982b27ff7',1,'ev::Rect3_::contains()'],['../structev_1_1Circ__.html#a6287aaef59e3f5f63c32c8ec920a65a1',1,'ev::Circ_::contains()'],['../classev_1_1PointCloud__.html#a025d618422c0e4ff20f4cd86268e482c',1,'ev::PointCloud_::contains()']]],
   ['count_3',['count',['../classev_1_1AbstractReader__.html#ab0ff8f66fecf9da41984df80e85ac49a',1,'ev::AbstractReader_::count()'],['../classev_1_1AbstractRepresentation__.html#af8063db8133182a4e6986d4dbe0ff5c5',1,'ev::AbstractRepresentation_::count()']]]
 ];
