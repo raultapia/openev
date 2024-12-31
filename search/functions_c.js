0a946397f218a3837aabf2827cab91b8e63f3881
raultapia <raultapia@users.noreply.github.com>
2024-12-31 13:07:59 +0000

    deploy: 5b8c543b910ae36a8df8fb3f64e53cb4d6f6b34f

--- search/functions_c.js
+++ search/functions_c.js
@@ -1,18 +1,19 @@
 var searchData=
 [
   ['setapstimeinterval_0',['setApsTimeInterval',['../classev_1_1Davis.html#ad86fffda93f5304b98314aafc8e71fa2',1,'ev::Davis']]],
-  ['setbias_1',['setBias',['../classev_1_1AbstractCamera.html#a7eea396e8cc99d7074ec6ae377f9223a',1,'ev::AbstractCamera::setBias()'],['../classev_1_1Davis.html#a71e2394a373d4d8d2609b20626df84f7',1,'ev::Davis::setBias(const uint8_t name, const BiasValue &amp;value)']]],
-  ['setdvseventsperpacket_2',['setDvsEventsPerPacket',['../classev_1_1Davis.html#a600bbd8f6582e5eb52fa4fd4241d8276',1,'ev::Davis']]],
-  ['setdvstimeinterval_3',['setDvsTimeInterval',['../classev_1_1Davis.html#a74c9a189b351a1e7be42b5813913a5a4',1,'ev::Davis']]],
-  ['setexposure_4',['setExposure',['../classev_1_1Davis.html#ab4f29d8847c55d95b179f8cc0a05f306',1,'ev::Davis']]],
-  ['setroi_5',['setRoi',['../classev_1_1AbstractCamera.html#a6d9fc3efdb25bb166b2aa685e40b197a',1,'ev::AbstractCamera']]],
-  ['settimeoffset_6',['setTimeOffset',['../classev_1_1AbstractCamera.html#abb4deb2bf57885b4e5847f613c67c538',1,'ev::AbstractCamera::setTimeOffset()'],['../classev_1_1AbstractRepresentation__.html#ad04186804acb170ece2da3d625d4461b',1,'ev::AbstractRepresentation_::setTimeOffset(const Event_&lt; E &gt; &amp;e)']]],
-  ['setvalue_7',['setValue',['../classev_1_1AbstractRepresentation__.html#a965b12576ace99d7dcabc261ec1adf2e',1,'ev::AbstractRepresentation_::setValue(const bool polarity, const Type &amp;value)'],['../classev_1_1AbstractRepresentation__.html#ae5d5697792a2ad033a67b42ec63b3aea',1,'ev::AbstractRepresentation_::setValue(const Type &amp;value)']]],
-  ['setvalues_8',['setValues',['../classev_1_1AbstractRepresentation__.html#ac3c17c53eae945fa6ff350b913e920de',1,'ev::AbstractRepresentation_']]],
-  ['size_9',['size',['../classev_1_1Rect3__.html#a4e05b496eed825b98c3663cd65b074ee',1,'ev::Rect3_']]],
-  ['size3_5f_10',['Size3_',['../classev_1_1Size3__.html#acb64ddf35ed140da98a834cff02a0740',1,'ev::Size3_::Size3_()'],['../classev_1_1Size3__.html#a9022d8414c3ab5c7cbf5bea98e8f60d1',1,'ev::Size3_::Size3_(T w, T h, T l)']]],
-  ['skip_5fn_11',['skip_n',['../classev_1_1AbstractReader__.html#a9a6740830a4b7e132aa83ab3ed96689a',1,'ev::AbstractReader_']]],
-  ['skip_5ft_12',['skip_t',['../classev_1_1AbstractReader__.html#a03abce7946e422c4e3c84adebe8c3ffb',1,'ev::AbstractReader_']]],
-  ['start_13',['start',['../classev_1_1AbstractCamera.html#a772cbc6ed1b40c183fdc5423abd6ade0',1,'ev::AbstractCamera']]],
-  ['stop_14',['stop',['../classev_1_1AbstractCamera.html#ad53b72c2d823eb43d6770660eb8c5296',1,'ev::AbstractCamera']]]
+  ['setbias_1',['setBias',['../classev_1_1AbstractCamera.html#a7eea396e8cc99d7074ec6ae377f9223a',1,'ev::AbstractCamera::setBias()'],['../classev_1_1Davis.html#a71e2394a373d4d8d2609b20626df84f7',1,'ev::Davis::setBias()']]],
+  ['setcolormap_2',['setColormap',['../classev_1_1AbstractRepresentation__.html#a6c04ab1aacaec2b1567760c3100ecd91',1,'ev::AbstractRepresentation_']]],
+  ['setdvseventsperpacket_3',['setDvsEventsPerPacket',['../classev_1_1Davis.html#a600bbd8f6582e5eb52fa4fd4241d8276',1,'ev::Davis']]],
+  ['setdvstimeinterval_4',['setDvsTimeInterval',['../classev_1_1Davis.html#a74c9a189b351a1e7be42b5813913a5a4',1,'ev::Davis']]],
+  ['setexposure_5',['setExposure',['../classev_1_1Davis.html#ab4f29d8847c55d95b179f8cc0a05f306',1,'ev::Davis']]],
+  ['setroi_6',['setRoi',['../classev_1_1AbstractCamera.html#a6d9fc3efdb25bb166b2aa685e40b197a',1,'ev::AbstractCamera']]],
+  ['settimeoffset_7',['setTimeOffset',['../classev_1_1AbstractCamera.html#abb4deb2bf57885b4e5847f613c67c538',1,'ev::AbstractCamera::setTimeOffset()'],['../classev_1_1AbstractRepresentation__.html#ad04186804acb170ece2da3d625d4461b',1,'ev::AbstractRepresentation_::setTimeOffset(const Event_&lt; E &gt; &amp;e)']]],
+  ['setvalue_8',['setValue',['../classev_1_1AbstractRepresentation__.html#a965b12576ace99d7dcabc261ec1adf2e',1,'ev::AbstractRepresentation_::setValue(const bool polarity, const Type &amp;value)'],['../classev_1_1AbstractRepresentation__.html#ae5d5697792a2ad033a67b42ec63b3aea',1,'ev::AbstractRepresentation_::setValue(const Type &amp;value)']]],
+  ['setvalues_9',['setValues',['../classev_1_1AbstractRepresentation__.html#ac3c17c53eae945fa6ff350b913e920de',1,'ev::AbstractRepresentation_']]],
+  ['size_10',['size',['../classev_1_1Rect3__.html#a4e05b496eed825b98c3663cd65b074ee',1,'ev::Rect3_']]],
+  ['size3_5f_11',['Size3_',['../classev_1_1Size3__.html#acb64ddf35ed140da98a834cff02a0740',1,'ev::Size3_::Size3_()'],['../classev_1_1Size3__.html#a9022d8414c3ab5c7cbf5bea98e8f60d1',1,'ev::Size3_::Size3_(T w, T h, T l)']]],
+  ['skip_5fn_12',['skip_n',['../classev_1_1AbstractReader__.html#a9a6740830a4b7e132aa83ab3ed96689a',1,'ev::AbstractReader_']]],
+  ['skip_5ft_13',['skip_t',['../classev_1_1AbstractReader__.html#a03abce7946e422c4e3c84adebe8c3ffb',1,'ev::AbstractReader_']]],
+  ['start_14',['start',['../classev_1_1AbstractCamera.html#a772cbc6ed1b40c183fdc5423abd6ade0',1,'ev::AbstractCamera']]],
+  ['stop_15',['stop',['../classev_1_1AbstractCamera.html#ad53b72c2d823eb43d6770660eb8c5296',1,'ev::AbstractCamera']]]
 ];
